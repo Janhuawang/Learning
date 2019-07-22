4f1acca8ddd8d80bd58125e9f46476bc932eb808
wjh <jianhua@umu.com>
2019-07-22 19:38:13 +0800

    build project

---
+++ app/src/main/java/com/learn/main/MainActivity.java
@@ -0,0 +1,38 @@
+package com.learn.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.learn.R;
+import com.learn.lock.LockActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<ItemBean> lists = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        lists.add(new ItemBean("线程Lock", LockActivity.class.getName()));
+    }
+
+    private void initView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, lists);
+        recyclerView.addItemDecoration(new android.support.v7.widget.DividerItemDecoration(this, android.support.v7.widget.DividerItemDecoration.VERTICAL));   // 设置分割线使用的divider
+        recyclerView.setAdapter(itemAdapter);
+    }
+}
