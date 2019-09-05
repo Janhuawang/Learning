0f7cf115fcf2bb7dc2e907db4097fdf112c2b448
wjh <jianhua@umu.com>
2019-09-05 20:43:45 +0800

    版本

---
+++ app/src/main/java/okhttp3/internal/Version.java
@@ -0,0 +1,14 @@
+package okhttp3.internal;
+
+/**
+ * 作者：wjh on 2019-09-05 20:15
+ */
+public final class Version {
+    public static String userAgent() {
+        return "okhttp/3.11.0";
+    }
+
+    private Version() {
+    }
+}
+
