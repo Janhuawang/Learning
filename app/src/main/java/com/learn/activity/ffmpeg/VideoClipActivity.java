c71e118899028e7c12e8a9f95d52bac400ad178e
wjh <jianhua@umu.com>
2019-10-11 18:53:10 +0800

    Permission权限问题 导致视频文件打不开，纠缠了很久。 - Android6.0（API 级别 23）开始，所有的权限都必须弹窗向用户索要，用户中间还可以手动关闭权限，所以首次打开app时需要申请全部权限，有以写重要的权限，可以在每次用到时检查并索要。

--- app/src/main/java/com/learn/activity/ffmpeg/VideoClipActivity.java
+++ app/src/main/java/com/learn/activity/ffmpeg/VideoClipActivity.java
@@ -1,7 +1,13 @@
 package com.learn.activity.ffmpeg;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.esay.ffmtool.FfmpegTool;
 import com.learn.R;
@@ -18,12 +24,13 @@ import java.util.concurrent.Executors;
  * 作者：wjh on 2019-10-10 21:25
  */
 public class VideoClipActivity extends BaseActivity {
-
-    private final String videoPath = "/storage/emulated/0/DCIM/Camera/VID_20191010_185014.mp4";
-    private String outPutDir;
+    private final int IMAGE_NUM = 10;// 每一屏图片的数量
+    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1009;
+    private String videoPath = "/storage/emulated/0/DCIM/Camera/VID_20191010_185014.mp4";
+    private String outPutClipDir;
+    private String outPutImgDir;
     private int startTime = 0;
-    private int endTime = 5 * 1000;
-
+    private int endTime = 10;
     /**
      * 执行线程
      */
@@ -53,9 +60,66 @@ public class VideoClipActivity extends BaseActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: // 权限回来了
+                if (grantResults != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initData();
+                }
+                break;
+        }
+    }
+
+    @Override
     protected void initData() {
-        outPutDir = FileUtil.getDCIMPath(this, "clip");
+        /**
+         * Android6.0（API 级别 23）开始，所有的权限都必须弹窗向用户索要，用户中间还可以手动关闭权限，
+         * 所以首次打开app时需要申请全部权限，有以写重要的权限，可以在每次用到时检查并索要。
+         */
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) { // 如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true
+                ToastUtil.showText("如果用户拒绝过一次，那就给一个申请权限的解释吧！");
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+            return;
+        }
+        if (!new File(videoPath).exists()) {
+            Toast.makeText(this, "视频文件不存在", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        outPutClipDir = FileUtil.getDCIMPath(this, "clip");
+        File file = new File(outPutClipDir);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        outPutImgDir = FileUtil.getDCIMPath(this, "img");
+        File fileImg = new File(outPutImgDir);
+        if (!fileImg.exists()) {
+            fileImg.mkdirs();
+        }
+
         ffmpegTool = FfmpegTool.getInstance(this);
+        ffmpegTool.setImageDecodeing(new FfmpegTool.ImageDecodeing() {
+            @Override
+            public void sucessOne(String s, int i) {
+                ToastUtil.showText("s---" + s);
+            }
+        });
+
+        runImagDecodTask(0, 5 * IMAGE_NUM);
     }
 
     @Override
@@ -66,7 +130,8 @@ public class VideoClipActivity extends BaseActivity {
             executorService.execute(new Runnable() {
                 @Override
                 public void run() {
-                    String video = outPutDir + File.separator + "clip" + System.currentTimeMillis() / 1000 + ".mp4";
+                    String video = outPutClipDir + File.separator + "clip" + System.currentTimeMillis() / 1000 + ".mp4";
+                    //参数说明 视频源  输出结果地址 开始时间单位s  视频时长单位s  标志位  回调
                     ffmpegTool.clipVideo(videoPath, video, startTime, endTime - startTime, 2, new FfmpegTool.VideoResult() {
                         @Override
                         public void clipResult(int i, String s, String s1, boolean b, int i1) {
@@ -79,5 +144,20 @@ public class VideoClipActivity extends BaseActivity {
                 }
             });
         }
+    }
+
+    /**
+     * 运行一个图片的解码任务
+     *
+     * @param start 解码开始的视频时间 秒
+     * @param count 一共解析多少张
+     */
+    private void runImagDecodTask(final int start, final int count) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                ffmpegTool.decodToImageWithCall(videoPath, outPutImgDir + "/", start, count);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/learn/util/UriUtil.java
@@ -0,0 +1,45 @@
+package com.learn.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+/**
+ * 路径
+ * 作者：wjh on 2018/11/23 21:44
+ * <p>
+ * Android 7.0强制启用了被称作 StrictMode的策略，带来的影响就是你的App对外无法暴露file://类型的URI了。
+ * 如果你使用Intent携带这样的URI去打开外部App(比如：打开系统相机拍照)，那么会抛出FileUriExposedException异常。
+ * <p>
+ * 官方给出解决这个问题的方案，就是使用FileProvider 使用步骤： 见：https://www.jianshu.com/p/61c09a321f2d
+ * 1.manifest中申明FileProvider
+ * 2.res/xml中定义对外暴露的文件夹路径
+ * 3.生成content://类型的Uri
+ * 4.给Uri授予临时权限
+ * 5.使用Intent传递Uri
+ */
+public class UriUtil {
+
+    public static Uri fromFile(Context context, File file) {
+        if (context == null || file == null) {
+            return null;
+        }
+        Uri fileUri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // 对应的是Manifest定义的 authorities
+            fileUri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", file);
+        } else {
+            fileUri = Uri.fromFile(file);
+        }
+        context.grantUriPermission(context.getPackageName(), fileUri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        context.grantUriPermission(context.getPackageName(), fileUri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        return fileUri;
+    }
+
+}
